a898ad453ad481088dacc3ef3a37baa58031876b
Kedar Iyer <kedarmail@gmail.com>
2017-09-06 12:56:55 -0400

    hello world compiles

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.15;
+
+contract HelloWorld {
+    address owner;
+    string greeting = "Hello World";
+
+    // Constructor function
+    // Runs only once, when contract is deployed
+    // Cannot be invoked after contract deployment
+    function HelloWorld () public {
+        owner = msg.sender;
+    }
+
+    function greet () constant public returns (string) {
+        return greeting;    
+    }
+
+    function kill () public {
+        require(owner == msg.sender);
+        selfdestruct(owner);
+    }
+}
