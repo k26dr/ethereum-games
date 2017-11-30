d4bd5baac48075b8103a70d181142de9595d31a6
Kedar Iyer <kedarmail@gmail.com>
2017-11-30 09:45:40 -0800

    gradual ponzi

--- contracts/PonzisAndPyramids.sol
+++ contracts/PonzisAndPyramids.sol
@@ -22,18 +22,30 @@ contract SimplePonzi {
 
 contract GradualPonzi {
     address[] public investors;
-    
+    mapping (address => uint) public balances;
+    uint public constant MINIMUM_INVESTMENT = 1e15;
+
+    function GradualPonzi () public {
+        investors.push(msg.sender);
+    }
+
     function () public payable {
+        require(msg.value >= MINIMUM_INVESTMENT);
         uint eachInvestorGets = msg.value / investors.length;
         for (uint i=0; i < investors.length; i++) {
-            investors[i].send(eachInvestorGets);
+            balances[investors[i]] += eachInvestorGets;
         }
         investors.push(msg.sender);
+    }
+
+    function withdraw () public {
+        uint payout = balances[msg.sender];
+        balances[msg.sender] = 0;
+        msg.sender.transfer(payout);
     }
 }
 
 contract SimplePyramid {
-     
     uint public constant MINIMUM_INVESTMENT = 1e15; // 0.001 ether
     uint public numInvestors = 0;
     uint public depth = 0;
