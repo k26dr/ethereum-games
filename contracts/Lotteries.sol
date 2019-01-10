3128272012f7dc6678332533aa97bad29411ba62
kedar <kedarmail@gmail.com>
2019-01-09 17:35:08 -0800

    bug fix to 96a5366 which closes #4

--- contracts/Lotteries.sol
+++ contracts/Lotteries.sol
@@ -201,7 +201,7 @@ contract Powerball {
     uint public constant TICKET_PRICE = 2e15;
     uint public constant MAX_NUMBER = 69;
     uint public constant MAX_POWERBALL_NUMBER = 26;
-    uint public constant ROUND_LENGTH = 15 seconds;
+    uint public constant ROUND_LENGTH = 3 days;
 
     uint public round;
     mapping(uint => Round) public rounds;
